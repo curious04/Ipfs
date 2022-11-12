10aae46fbee378fc05386608baf13d094a61442c
Hrithik Singh <hrithikks10@gmail.com>
2022-11-12 20:08:47 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import fs from 'fs'
 import Uploader from './Uploader';
-
+import Navbar from './Navbar'
 
 function App() {
   return (
    <>
 
- 
- 
+ <div className='App'>
+<Navbar /> 
    <Uploader/>
-   
+   </div>
    </>
   );
 }
