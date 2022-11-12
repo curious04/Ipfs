d94a9c0edea32989f208f57e5b4e28c9b673b921
Hrithik Singh <hrithikks10@gmail.com>
2022-11-12 20:12:50 +0530

    files deleted

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,12 @@
 import React, { useState } from "react";
 import Logo from "./assets/chains.png";
-import { Link } from "react-router-dom";
-import ReorderIcon from "@material-ui/icons/Reorder";
 import "./Navbar.css";
 
 function Navbar() {
-  const [openLinks, setOpenLinks] = useState(false);
 
-  const toggleNavbar = () => {
-    setOpenLinks(!openLinks);
-  };
   return (
     <div className="navbar">
-      <div className="leftSide" id={openLinks ? "open" : "close"}>
+      <div className="leftSide" >
         <img src={Logo} />
       </div>
     </div>
